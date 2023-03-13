1bb8f313497bde7ccfd423358f66058d469fdbf8
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-03-13 07:35:48 +0200

    Task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const getIframe = document.querySelector('iframe');
+const player = new Player(getIframe);
+
+const KEY_STORAGE = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(function (currentTime) {
+    const seconds = currentTime.seconds;
+    localStorage.setItem(KEY_STORAGE, JSON.stringify(seconds));
+    console.log(localStorage);
+  }),
+  1000
+);
+
+const timeOn = JSON.parse(localStorage.getItem(KEY_STORAGE));
+
+player
+  .setCurrentTime(timeOn)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
