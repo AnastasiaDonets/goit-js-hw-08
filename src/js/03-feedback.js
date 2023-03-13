de87fd0d5f89ccaa681cd99cee802aead234b448
AnastasiaDonets <anastasia.cheban@outlook.com>
2023-03-13 09:05:51 +0200

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]');
+const textarea = document.querySelector('[name="message"]');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onSaveData, 500));
+
+const formData = {};
+
+const KEY_STORAGE = 'feedback-form-state';
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY_STORAGE);
+  console.log(formData);
+}
+
+function onSaveData(e) {
+  formData.email = input.value;
+  formData.message = textarea.value;
+
+  localStorage.setItem(KEY_STORAGE, JSON.stringify(formData));
+}
+
+populateMessage();
+
+function populateMessage() {
+  const savedData = JSON.parse(localStorage.getItem(KEY_STORAGE));
+  if (savedData) {
+    input.value = savedData.email;
+    textarea.value = savedData.message;
+  }
+}
